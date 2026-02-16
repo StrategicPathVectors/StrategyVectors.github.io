71f9ae18d9b8583a3c8c512df80a61d1b2e9b2fe
Robin Maiden <robin@consultchameleon.co.uk>
2026-02-16 11:09:25 +0000

    Update videos-data.js

--- js/videos-data.js
+++ js/videos-data.js
@@ -5,40 +5,22 @@
 
 const videosData = [
     {
-        id: "video-7",
-        museId: "9pvgqUT",
-        title: "Obesity in Ireland; NEW Patient Journey Framework summary - as a newsroom headlines feature",
-        description: "Our strategy creation tool pinpoints system-level issues & creates a series of Strategy Vectors (pivot points) for strategy creation & discussion stimuli",
-        date: "November 2025",
-        duration: "8 mins",
-        dateOrder: 20251117
+        id: "video-3",
+        museId: "UVWfqS4",
+        title: "Advert: Building a Patient Journey Database for client interrogation",
+        description: "We're constantly creating new content. Check back soon for more on Strategy Vectors & The Patient Journey Framework",
+        date: "February 2026",
+        duration: "<2 min",
+        dateOrder: 20260210
     },
     {
-        id: "video-6",
-        museId: "4HfRzGn",
-        title: "Psoriasis in Sweden; a Patient Journey Framework summary AND patient story",
-        description: "Our new strategy creation tool has been used in many markets, many conditions & therapy areas; here is yet another example",
-        date: "July 2025",
-        duration: "4+ mins",
-        dateOrder: 20250817
-    },
-    {
-        id: "video-5",
-        museId: "nKsiPzj",
-        title: "One Italian Alzheimer's Disease Patient Segment",
-        description: "See how our Unified Patient journey Framework can aid identification of interesting & underserved patient segments; this is a very early AI-version (much improved subsequently)",
-        date: "June 2025",
-        duration: "3 mins",
-        dateOrder: 20250617
-    },
-    {
-        id: "video-4",
-        museId: "HiXnF9n",
-        title: "MASH Crisis in Spain",
-        description: "See how our Unified Patient journey Framework identifies critical intelligence, MASH system failures, drives fresh insights & stimulates new strategic discussions",
-        date: "November 2025",
-        duration: "7 mins",
-        dateOrder: 20251107
+        id: "video-10",
+        museId: "kDMwbjp",
+        title: "Launch Team Leader gives their team an EXTRA 12 MONTHS by using Strategy Vectors & Patient Journey Framework",
+        description: "A new hire experienced launch leader faced the daunting task of rapidly understanding their market to catch-up to where their new launch should've already been. They got back on track & better aligned their team with us...",
+        date: "December 2025",
+        duration: "3 min",
+        dateOrder: 20251215
     },
     {
         id: "video-1",
@@ -59,25 +41,43 @@ const videosData = [
         dateOrder: 20251119
     },
     {
-        id: "video-3",
-        museId: "UVWfqS4",
-        title: "Advert: Building a Patient Journey Database for client interrogation",
-        description: "We're constantly creating new content. Check back soon for more on Strategy Vectors & The Patient Journey Framework",
-        date: "February 2026",
-        duration: "<2 min",
-        dateOrder: 20260210,
-    }
+        id: "video-7",
+        museId: "9pvgqUT",
+        title: "Obesity in Ireland; NEW Patient Journey Framework summary - as a newsroom headlines feature",
+        description: "Our strategy creation tool pinpoints system-level issues & creates a series of Strategy Vectors (pivot points) for strategy creation & discussion stimuli",
+        date: "November 2025",
+        duration: "8 mins",
+        dateOrder: 20251117
+    },
     {
-        id: "video-10",
-        museId: "kDMwbjp",
-        title: "Launch Team Leader gives their team an EXTRA 12 MONTHS by using Strategy Vectors & Patient Journey Framework",
-        description: "A new hire experienced launch leader faced a the daunting task of rapidly understanding their market to "catch-up" to where their new launch should've already been. They got back on track & better aligned their team with us...",
-        date: "December 2025",
-        duration: "3 min",
-        dateOrder: 20250901,
-    }
-{
-        id: "video-10",
+        id: "video-4",
+        museId: "HiXnF9n",
+        title: "MASH Crisis in Spain",
+        description: "See how our Unified Patient journey Framework identifies critical intelligence, MASH system failures, drives fresh insights & stimulates new strategic discussions",
+        date: "November 2025",
+        duration: "7 mins",
+        dateOrder: 20251107
+    },
+    {
+        id: "video-6",
+        museId: "4HfRzGn",
+        title: "Psoriasis in Sweden; a Patient Journey Framework summary AND patient story",
+        description: "Our new strategy creation tool has been used in many markets, many conditions & therapy areas; here is yet another example",
+        date: "July 2025",
+        duration: "4+ mins",
+        dateOrder: 20250817
+    },
+    {
+        id: "video-5",
+        museId: "nKsiPzj",
+        title: "One Italian Alzheimer's Disease Patient Segment",
+        description: "See how our Unified Patient journey Framework can aid identification of interesting & underserved patient segments; this is a very early AI-version (much improved subsequently)",
+        date: "June 2025",
+        duration: "3 mins",
+        dateOrder: 20250617
+    },
+    {
+        id: "video-11",
         museId: "PLACEHOLDER",
         title: "Future Content",
         description: "We're constantly creating new content. Check back soon for more on Strategy Vectors & The Patient Journey Framework",
